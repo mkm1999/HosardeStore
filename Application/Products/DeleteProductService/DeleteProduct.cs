c16b8e9222de0d52a6ab9713f508365edf2435a3
Mohammad <mohammadkarbalaee98@gmail.com>
2023-09-04 21:51:25 +0330

    refactor : Edit Product Delete for delete files

--- Application/Products/DeleteProductService/DeleteProduct.cs
+++ Application/Products/DeleteProductService/DeleteProduct.cs
@@ -1,19 +1,34 @@
 ﻿using Application.Interfaces.Context;
 using Common;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using System.Linq;
 
 namespace Application.Products.DeleteProductService
 {
     public class DeleteProduct : IDeleteProduct
     {
         private readonly IDataBaseContext _context;
+        private readonly IHostingEnvironment _environment;
 
-        public DeleteProduct(IDataBaseContext context)
+        public DeleteProduct(IDataBaseContext context, IHostingEnvironment environment)
         {
             _context = context;
+            _environment = environment;
         }
 
         public ResultDto Execute(int ProductId)
         {
+            var productImages = _context.productsImages.Where(p => p.ProductId == ProductId).ToList();
+            if(productImages.Count > 0)
+            {
+                foreach (var image in productImages)
+                {
+                    string path = _environment.WebRootPath + image.Url;
+                    File.Delete(path);
+                }
+
+            }
             var product = _context.products.Find(ProductId);
             if (product == null)
             {
