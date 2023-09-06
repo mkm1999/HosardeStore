ebe804c107ea817da99f22ef462526d78a8c656a
Mohammad <mohammadkarbalaee98@gmail.com>
2023-09-06 19:39:21 +0330

    feat : Add ProductDetails Services

--- UI/Controllers/ProductsController.cs
+++ UI/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using Application.Products.GetProductsForSiteService;
+﻿using Application.Products.FindProductService;
+using Application.Products.GetProductsForSiteService;
 using Microsoft.AspNetCore.Mvc;
 
 namespace UI.Controllers
@@ -6,16 +7,24 @@ namespace UI.Controllers
     public class ProductsController : Controller
     {
         private readonly IGetProductsForSite getProductsForSite;
+        private readonly IFindProduct findProduct;
 
-        public ProductsController(IGetProductsForSite getProductsForSite)
+        public ProductsController(IGetProductsForSite getProductsForSite, IFindProduct findProduct)
         {
             this.getProductsForSite = getProductsForSite;
+            this.findProduct = findProduct;
         }
 
         public IActionResult Index(string SearchKey)
         {
             var products = getProductsForSite.Execute(SearchKey);
             return View(products);
+        }
+
+        public IActionResult Details(int Id)
+        {
+            var product = findProduct.FindWithId(Id);
+            return View(product);
         }
     }
 }
