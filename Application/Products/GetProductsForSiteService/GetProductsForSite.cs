9e638fed4654fd2fe4fcd6461f3414bb7a1ba160
Mohammad <mohammadkarbalaee98@gmail.com>
2023-09-13 03:24:25 +0330

    feat : Add Searching Service

--- Application/Products/GetProductsForSiteService/GetProductsForSite.cs
+++ Application/Products/GetProductsForSiteService/GetProductsForSite.cs
@@ -17,18 +17,21 @@ namespace Application.Products.GetProductsForSiteService
         public List<GetProductsForSiteDto> Execute(string SearchKey , int? CategoryId = null)
         {
             var ProductsQuery = _context.products.Include(p => p.Images).Include(p => p.Category).ThenInclude(c => c.ParentCategory).AsQueryable();
-            if(! string.IsNullOrEmpty(SearchKey))
-            {
-                ProductsQuery = ProductsQuery.Where(p => p.Name.Contains(SearchKey) || p.Barnd.Contains(SearchKey) || p.Category.Name.Contains(SearchKey)).AsQueryable();
-            }
-            if(CategoryId != null)
+
+            if (CategoryId != null)
             {
                 ProductsQuery = ProductsQuery.Where(
-                    p => p.CategoryId ==  CategoryId ||
+                    p => p.CategoryId == CategoryId ||
                     p.Category.ParentCategoryId == CategoryId ||
                     p.Category.ParentCategory.ParentCategoryId == CategoryId
                     ).AsQueryable();
             }
+
+            if (! string.IsNullOrEmpty(SearchKey))
+            {
+                ProductsQuery = ProductsQuery.Where(p => p.Name.Contains(SearchKey) || p.Barnd.Contains(SearchKey) || p.Category.Name.Contains(SearchKey)).AsQueryable();
+            }
+            
             var Products = ProductsQuery.Select(p => new GetProductsForSiteDto
             {
                 ImgSrc = p.Images.FirstOrDefault().Url,
---
+++ UI/ViewComponents/SearchCategoriesViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Application.Categories.GetCategoriesService;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+
+namespace UI.ViewComponents
+{
+    public class SearchCategoriesViewComponent : ViewComponent
+    {
+        private readonly IGetCategories getCategories;
+
+        public SearchCategoriesViewComponent(IGetCategories getCategories)
+        {
+            this.getCategories = getCategories;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categoriesList = getCategories.Execute(null);
+            var categories = new SelectList(categoriesList, "Id", "Name");
+            return View("View",categories);
+        }
+    }
+}
