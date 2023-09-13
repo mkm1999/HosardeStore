d1ab78d26fa3504821be6926fd186e7e9f8a3edf
Mohammad <mohammadkarbalaee98@gmail.com>
2023-09-13 15:53:06 +0330

    feat : small get menu

---
+++ UI/ViewComponents/SmallGetMenuViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Application.Categories.GetFullCategroiesService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UI.ViewComponents
+{
+    public class SmallGetMenuViewComponent : ViewComponent
+    {
+        private readonly IGetFullCategories getFullCategories;
+
+        public SmallGetMenuViewComponent(IGetFullCategories getFullCategories)
+        {
+            this.getFullCategories = getFullCategories;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = getFullCategories.Execute();
+            return View("View", categories);
+        }
+    }
+}
